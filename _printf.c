3be1e9437c0095917b7c79000ecd5f688f79863d
princexz <prince_solomon@yahoo.com>
2023-01-25 16:15:50 -0800

    Alx-Prinf_project

--- _printf.c
+++ _printf.c
@@ -1,185 +1,50 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * _printf - Simulates the printf function
- * @format: Format to print to the console
- *
- * Description: Function that simulate the printf function
- * for the following conversion specifiers:
- *    - c:    Chracter
- *    - s/S:  Strings. 's' prints the same string.
- *            'S' prints the string, changing the non-printable characters
- *            to '\x??' format, where '??' corresponds to a hexadecimal number
- *    - d/i:  Integers
- *    - o:    Octal numbers
- *    - x/X:  Hexadecimal numbers
- *    - b:    Binary numbers
- *    - u:    Unsigned numbers
- *    - %:    '%' char
- *    - r:    Reverse string
- *    - R:    Root13 of the string
- * The function considerate the next arguments, checking the usability for the
- * conversion specifier used:
- * (+)      Adds a '+' sign to positive integers
- * ( )      Adds a ' ' to the start of the number
- * (#)      Adds '0' or '0x' to the start of the octal or hexadecimal number
- * (l)      Converts the number to a 'long' type
- * (h)      Converts the number to a 'short' type
- * (width)  Add spaces(?) to the printing format
- * (preci)  Print the number with the corresponding precision
- * (0)      Add '0' to the spaces created with 'width'
- * (-)      Justify the string to the left
- *
- * Return: Number of characters printed
- * (excluding the null byte used to end output to strings)
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	va_list list;
-	char *buffer, *str;
-	int i = 0, i_buffer = 0, j_spec, lenstr, stock = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	buffer = malloc(1024);
-	if (buffer == NULL)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(list, format);
-	while (format && format[i])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] == '%')
+		if (*p == '%')
 		{
-			if (check_specs(format + i, &j_spec) == 1
-			    && check_format(format + i, j_spec))
+			p++;
+			if (*p == '%')
 			{
-				str = generate_malloc(format + i, j_spec, list, &lenstr);
-				lenstr = format[i + j_spec - 1] == 'c' ? lenstr : _strlen(str);
-				if (str == NULL)
-					return (free_buffer(buffer, i_buffer));
-				_memcpy(buffer, str, &i_buffer, &stock, lenstr), free(str);
-				i += j_spec;
+				count += _putchar('%');
 				continue;
 			}
-			if (check_specs(format + i, &j_spec) == -1)
-				return (free_buffer(buffer, i_buffer));
-			if (format[i + 1] == 'h' || format[i + 1] == 'l')
-				_memcpy(buffer, format + i, &i_buffer, &stock, 1), j_spec = 2;
-			else
-				_memcpy(buffer, format + i, &i_buffer, &stock, j_spec);
-			i += j_spec;
-			continue;
-		}
-		_memcpy(buffer, format + i, &i_buffer, &stock, 1), i++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	va_end(list), i_buffer = write(1, buffer, i_buffer), free(buffer);
-	return (i == 0 && !format ? -1 : stock + i_buffer);
-}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 
-/**
- * check_specs - Check if a string has a conversion specifier
- * @s: String where to check the conversion specifier
- * @p: Pointer to the variable where the length to consider is saved
- * Return: -1 if ends the string
- * 0 if does not find a conversion specifier but does not end the format
- * 1 if find a conversition specifier
- */
-int check_specs(const char *s, int *p)
-{
-	char *specs = "%csSdioxXburRp";
-	char *flags = " 0-+#123456789.lh*";
-	int i = 0, j = 0, len_flags = _strlen(flags);
-
-	while (s[i])
-	{
-		j = 0;
-		while (specs[j])
-		{
-			if (s[i] == specs[j] && i != 0)
-			{
-				*p = i + 1;
-				return (1);
-			}
-			j++;
-		}
-		j = 0;
-		while (flags[j])
-		{
-			if (s[i] == flags[j])
-				break;
-			j++;
-		}
-		if (i > 0 && j == len_flags)
-			break;
-		i++;
-	}
-	if (!s[i])
-		return (-1);
-	*p = i;
-	return (0);
-}
-
-
-/**
- * _memcpy - copies the memory from src to the buffer
- * @buffer: The destination pointer
- * @src: The source pointer
- * @i_b: Pointer to the index of the buffer
- * @stock: Pointer to the actual stock of bytes written
- * @n: bytes to use from src
- *
- * Description: Copies the memory from src to the buffer inserted.
- * the buffer has a limit of 1024. If the index of buffer is greater than
- * 1024 after the copy, writes the content to the standard output, sum the
- * bytes written and resets the index
- *
- * Return: Nothing(?)
- */
-void _memcpy(char *buffer, const char *src, int *i_b, int *stock, int n)
-{
-	int i = 0;
-
-	while (i < n)
-	{
-		*(buffer + *i_b) = *(src + i);
-		i++;
-		*i_b = *i_b + 1;
-		if (*i_b > 1024)
-		{
-			*stock = *stock + *i_b;
-			write(1, buffer, *i_b);
-			*i_b = 0;
-		}
-	}
-}
-
-/**
- * _strlen - return the length of a string
- * @s: Pointer to an string
- *
- * Return: Length of the string
- * If is a null pointer returns 0
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (s == NULL)
-		return (0);
-
-	while (*(s + i) != '\0')
-		i++;
-
-	return (i);
-}
-
-/**
- * free_buffer - Writes the content of the buffer and frees it
- * @buff: Buffer to free
- * @len: Bytes to write
- *
- * Return: Always -1 (Success)
- */
-int free_buffer(char *buff, int len)
-{
-	write(1, buff, len);
-	free(buff);
-	return (-1);
 }
--- main.h
+++ main.h
@@ -1,76 +1,76 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdarg.h>
 
 /**
- * struct Checkstr - Struct to check format
- *
- * @flag: character associated to flags
- * @flagnot: Flags that are not allowed per specifier
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
-typedef struct Checkstr
+typedef struct flags
 {
-	char flag;
-	char *flagnot;
-} checkstr;
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
 /**
- * struct Specs_t - struct to call specifier functions
- *
- * @s: character representing specifier
- * @f: The function associated
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
  */
-typedef struct Specs_t
+typedef struct printHandler
 {
-	char s;
-	char *(*f)(const char *, int, va_list, int *);
-} specs_t;
-/* Custom printf function */
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
 int _printf(const char *format, ...);
 
-/* Auxiliar functions */
-int _strlen(char *s);
-int blen(unsigned long int n, unsigned long int base);
-int blen10(long int n, long int base);
-void rev_str(char *s);
-char *hexS(int n);
-char *rot13(char *s);
-int check_format(const char *formspec, int j);
-int check_specs(const char *s, int *p);
-void _memcpy(char *buffer, const char *src, int *i_b, int *stock, int n);
-void print_number_str(long int n, char *str);
-void print_number_str_u(unsigned long int n, char *str);
-int free_buffer(char *, int);
-int effective_length(char *buffer);
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
 
-/* Generate strings functions */
-char *generate_malloc(const char *s, int len_p, va_list list, int *);
-char *gen_c(const char *, int, va_list, int *);
-char *gen_s(const char *, int, va_list, int *);
-char *gen_d(const char *, int, va_list, int *);
-char *gen_i(const char *, int, va_list, int *);
-char *gen_o(const char *, int, va_list, int *);
-char *gen_b(const char *, int, va_list, int *);
-char *gen_x(const char *, int, va_list, int *);
-char *gen_X(const char *, int, va_list, int *);
-char *gen_p(const char *, int, va_list, int *);
-char *gen_u(const char *, int, va_list, int *);
-char *gen_S(const char *, int, va_list, int *);
-char *gen_r(const char *, int, va_list, int *);
-char *gen_R(const char *, int, va_list, int *);
-char *gen_per(const char *, int, va_list, int *);
-/* Flags functions */
-unsigned int get_width(const char *pattern, int len_p, va_list list);
-unsigned int get_precision(const char *pattern, int len_p, va_list list);
-long int get_param(const char *pattern, int len_p, va_list list);
-unsigned long int get_param_u(const char *pattern, int len_p, va_list list);
-void app_flags(const char *, int, char *, int *, int, int);
-int app_precision(const char *pattern, int len_p, char *buffer, int pr);
-int app_width(char *buffer, int len_b, int wi);
-int app_hash(const char *pattern, int len_p, char *buffer);
-int app_plus_space(const char *pattern, int len_p, char *buffer);
-int check_flag(const char *pattern, int len_p, char flag);
-#endif /** Princexz */
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
+#endif
